590bb4bd27b929cce88f8360dfaa7b0bdfdfaaf4
仓鼠 <zhuxx2003@gmail.com>
2023-12-30 01:25:23 +0800

    handle empty list

--- src/index.ts
+++ src/index.ts
@@ -17,8 +17,9 @@ const apiEndpointPrefix = 'https://www.googleapis.com/youtube/v3/videos';
 export function apply(context: Context, config: Config) {
   const ctx = context.isolate(['http'])
   ctx.http = context.http.extend(config.quester)
+  const logger = ctx.logger('youtube')
 
-  function MediaFormat (){
+  function MediaFormat() {
     // http://www.youtube.com/embed/m5yCOSHeYn4
     var ytRegEx = /^(?:https?:\/\/)?(?:i\.|www\.|img\.)?(?:youtu\.be\/|youtube\.com\/|ytimg\.com\/)(?:embed\/|v\/|vi\/|vi_webp\/|watch\?v=|watch\?.+&v=)((\w|-){11})(?:\S+)?$/;
     // http://vimeo.com/3116167, https://player.vimeo.com/video/50489180, http://vimeo.com/channels/3116167, http://vimeo.com/channels/staffpicks/113544877
@@ -28,44 +29,44 @@ export function apply(context: Context, config: Config) {
     // https://soundcloud.com/aviciiofficial/avicii-you-make-me-diplo-remix, https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/29395900&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false
     var scRegEx = /https?:\/\/(?:w\.|www\.|)(?:soundcloud\.com\/)(?:(?:player\/\?url=https\%3A\/\/api.soundcloud.com\/tracks\/)|)(((\w|-)[^A-z]{7})|([A-Za-z0-9]+(?:[-_][A-Za-z0-9]+)*(?!\/sets(?:\/|$))(?:\/[A-Za-z0-9]+(?:[-_][A-Za-z0-9]+)*){1,2}))/;
 
-    function getIDfromRegEx ( src, regExpy ){
-        return (src.match(regExpy)) ? RegExp.$1 : null;
+    function getIDfromRegEx(src, regExpy) {
+      return (src.match(regExpy)) ? RegExp.$1 : null;
     }
 
     return {
-        // returns only the ID
-        getYoutubeID: function ( src ){
-            return getIDfromRegEx( src, ytRegEx);
-        },
-        // returns main link
-        getYoutubeUrl: function ( ID ){
-            return "https://www.youtube.com/watch?v=" + ID;
-        },
-        // returns only the ID
-        getVimeoID: function ( src ){
-            return getIDfromRegEx( src, vmRegEx);
-        },
-        // returns main link
-        getVimeoUrl: function ( ID ){
-            return "http://vimeo.com/" + ID;
-        },
-        // returns only the ID
-        getSpotifyID: function ( src ){
-            return getIDfromRegEx( src, spRegEx);
-        },
-        // returns main link
-        getSpotifyUrl: function ( ID ){
-            return "http://open.spotify.com/track/" + ID;
-        },
-        // returns only the ID
-        getSoundcloudID: function ( src ){
-            return getIDfromRegEx( src, scRegEx);
-        },
-        // returns main link
-        // NOTE: this one really sucks since soundcloud doesnt have good API without js library
-        getSoundcloudUrl: function ( ID ){
-            return "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/" + ID; //only way to link to the track currently
-        }
+      // returns only the ID
+      getYoutubeID: function (src) {
+        return getIDfromRegEx(src, ytRegEx);
+      },
+      // returns main link
+      getYoutubeUrl: function (ID) {
+        return "https://www.youtube.com/watch?v=" + ID;
+      },
+      // returns only the ID
+      getVimeoID: function (src) {
+        return getIDfromRegEx(src, vmRegEx);
+      },
+      // returns main link
+      getVimeoUrl: function (ID) {
+        return "http://vimeo.com/" + ID;
+      },
+      // returns only the ID
+      getSpotifyID: function (src) {
+        return getIDfromRegEx(src, spRegEx);
+      },
+      // returns main link
+      getSpotifyUrl: function (ID) {
+        return "http://open.spotify.com/track/" + ID;
+      },
+      // returns only the ID
+      getSoundcloudID: function (src) {
+        return getIDfromRegEx(src, scRegEx);
+      },
+      // returns main link
+      // NOTE: this one really sucks since soundcloud doesnt have good API without js library
+      getSoundcloudUrl: function (ID) {
+        return "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/" + ID; //only way to link to the track currently
+      }
     };
   }
 
@@ -73,7 +74,7 @@ export function apply(context: Context, config: Config) {
     return await ctx.http.get(`${apiEndpointPrefix}?id=${id}&key=${config.apiKey}&part=snippet,contentDetails,statistics,status`);
   };
 
-  ctx.middleware(async (session, next, ) => {
+  ctx.middleware(async (session, next,) => {
     const isYoutube = session.content.includes('youtube.com') || session.content.includes('https://youtu.be')
     if (!isYoutube) return next()
 
@@ -81,11 +82,15 @@ export function apply(context: Context, config: Config) {
       let id;
       if (session.content.includes('https://youtu.be')) {
         const index = session.content.lastIndexOf("\/");
-        id = session.content.substring(index + 1,session.content.length);
+        id = session.content.substring(index + 1, session.content.length);
       } else {
         id = MediaFormat().getYoutubeID(session.content);
       }
       const result = await fetchDataFromAPI(id);
+      if (!result.pageInfo.totalResults) {
+        logger.warn('接口返回了空列表')
+        return '接口返回了空列表'
+      }
       const snippet = result.items[0].snippet;
       const {
         title,
@@ -113,8 +118,8 @@ export function apply(context: Context, config: Config) {
 发布时间: ${publishedAt}
 简介: ${description}
 ${segment.image(thumbnail, mime)}`;
-    } catch(err) {
-      console.log(err);
+    } catch (err) {
+      logger.warn(err);
       return `发生错误!;  ${err}`;
     }
   });
